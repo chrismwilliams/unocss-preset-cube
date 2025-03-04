1f7fa80d875585cca3d3c34981d3de4b8e0f5974
Chris Williams <chrismwilliams@users.noreply.github.com>
2025-03-04 13:58:21 +0000

    add option prefix to preset

--- src/index.ts
+++ src/index.ts
@@ -1,187 +1,185 @@
 import type { Preset } from "unocss";
 import type { CubePresetOptions } from "./types";
 
-const defaultCubePresetOptions: Required<CubePresetOptions> = {
-  blocksLayerName: "components",
-  compositionsLayerName: "components",
-  utilitiesLayerName: "components",
-  utilityClassesLayerName: "components",
+const defaultCubePresetOptions: CubePresetOptions = {
+	blocksLayerName: "components",
+	compositionsLayerName: "components",
+	utilitiesLayerName: "components",
+	utilityClassesLayerName: "components",
 };
 
-export function presetCube(options: CubePresetOptions): Preset {
-  const config: CubePresetOptions = {
-    ...defaultCubePresetOptions,
-    ...options,
-  };
-  return {
-    name: "unocss-preset-cube",
-    theme: {},
-    rules: [
-      // blocks
-      [
-        "prose",
-        {
-          "--flow-space": "var(--space-m)",
-        },
-        { layer: config.blocksLayerName },
-      ],
-      // compositions
-      [
-        "cluster",
-        {
-          display: "flex",
-          "flex-wrap": "wrap",
-          gap: "var(--gutter, var(--space-s-m))",
-          "justify-content": "var(--cluster-horizontal-alignment, flex-start)",
-          "align-items": "var(--cluster-vertical-alignment, center)",
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        /^flow$/,
-        (_, { symbols }) => {
-          return {
-            [symbols.selector]: (selector) => `${selector} > * + *`,
-            "margin-top": "var(--flow-space, 1em)",
-          };
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        /^grid$/,
-        function* (_, { symbols }) {
-          yield {
-            display: "grid",
-            "grid-template-columns":
-              "repeat(var(--grid-placement, auto-fill),minmax(var(--grid-min-item-size,16rem), 1fr))",
-            gap: "var(--gutter, var(--space-s-l))",
-          };
-          yield {
-            [symbols.selector]: (selector) =>
-              `${selector}[data-layout="halves"]`,
-            "--grid-placement": "auto-fit",
-            "--grid-min-item-size": "clamp(16rem, 50vw, 33rem)",
-          };
-          yield {
-            [symbols.selector]: (selector) =>
-              `${selector}[data-layout="thirds"]`,
-            "--grid-placement": "auto-fit",
-            "--grid-min-item-size": "clamp(16rem, 33%, 20rem)",
-          };
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        /^repel$/,
-        function* (_, { symbols }) {
-          yield {
-            display: "flex",
-            "flex-wrap": "wrap",
-            "justify-content": "space-between",
-            "align-items": "var(--repel-vertical-alignment, center)",
-            gap: " var(--gutter, var(--space-s-m))",
-          };
-          yield {
-            [symbols.selector]: (selector) => `${selector}[data-nowrap]`,
-            "flex-wrap": "nowrap",
-          };
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        /^sidebar$/,
-        function* (_, { symbols }) {
-          yield {
-            display: "flex",
-            "flex-wrap": "wrap",
-            gap: "var(--gutter, var(--space-s-l))",
-          };
-          yield {
-            [symbols.selector]: (selector) => `${selector} > :first-child`,
-            "flex-basis": "var(--sidebar-target-width, 20rem)",
-            "flex-grow": "1",
-          };
-          yield {
-            [symbols.selector]: (selector) => `${selector} > :last-child`,
-            "flex-basis": 0,
-            "flex-grow": 999,
-            "min-width": "var(--sidebar-content-min-width, 50%)",
-          };
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        /^switcher$/,
-        function* (_, { symbols }) {
-          yield {
-            display: "flex",
-            "flex-wrap": "wrap",
-            gap: "var(--gutter, var(--space-s-l))",
-            "align-items": "var(--switcher-vertical-alignment, flex-start)",
-          };
-          yield {
-            [symbols.selector]: (selector) => `${selector} > *`,
-            "flex-grow": "1",
-            "flex-basis":
-              "calc((var(--switcher-target-container-width, 40rem) - 100%) * 999)",
-          };
-          yield {
-            [symbols.selector]: (selector) => `${selector} > :nth-child(n + 3)`,
-            "flex-basis": "100%",
-          };
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      [
-        "wrapper",
-        {
-          "margin-inline": "auto",
-          "max-width": "clamp(16rem, var(--wrapper-max-width, 100vw), 80rem)",
-          "padding-left": "var(--gutter)",
-          "padding-right": "var(--gutter)",
-          position: "relative",
-        },
-        { layer: config.compositionsLayerName },
-      ],
-      // utilities
-      [
-        "region",
-        {
-          "padding-block": "var(--region-space, var(--space-xl-2xl))",
-        },
-        { layer: config.utilitiesLayerName },
-      ],
-      [
-        "visually-hidden",
-        {
-          border: "0",
-          clip: "rect(0 0 0 0)",
-          height: "0",
-          margin: "0",
-          overflow: "hidden",
-          padding: "0",
-          position: "absolute",
-          width: "1px",
-          "white-space": "nowrap",
-        },
-        { layer: config.utilitiesLayerName },
-      ],
-      // utility classes
-      [
-        /^flow-space-(.+)$/,
-        ([, d]) => ({ "--flow-space": `var(--space-${d})` }),
-        { layer: config.utilityClassesLayerName },
-      ],
-      [
-        /^region-space-(.+)$/,
-        ([, d]) => ({ "--region-space": `var(--space-${d})` }),
-        { layer: config.utilityClassesLayerName },
-      ],
-      [
-        /^gutter-(.+)$/,
-        ([, d]) => ({ "--gutter": `var(--space-${d})` }),
-        { layer: config.utilityClassesLayerName },
-      ],
-    ],
-  };
+export function presetCube(options: CubePresetOptions = {}): Preset {
+	const config: CubePresetOptions = {
+		...defaultCubePresetOptions,
+		...options,
+	};
+	return {
+		name: "unocss-preset-cube",
+		theme: {},
+		prefix: options.prefix,
+		rules: [
+			// blocks
+			[
+				"prose",
+				{
+					"--flow-space": "var(--space-m)",
+				},
+				{ layer: config.blocksLayerName },
+			],
+			// compositions
+			[
+				"cluster",
+				{
+					display: "flex",
+					"flex-wrap": "wrap",
+					gap: "var(--gutter, var(--space-s-m))",
+					"justify-content": "var(--cluster-horizontal-alignment, flex-start)",
+					"align-items": "var(--cluster-vertical-alignment, center)",
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				/^flow$/,
+				(_, { symbols }) => {
+					return {
+						[symbols.selector]: (selector) => `${selector} > * + *`,
+						"margin-top": "var(--flow-space, 1em)",
+					};
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				/^grid$/,
+				function* (_, { symbols }) {
+					yield {
+						display: "grid",
+						"grid-template-columns":
+							"repeat(var(--grid-placement, auto-fill),minmax(var(--grid-min-item-size,16rem), 1fr))",
+						gap: "var(--gutter, var(--space-s-l))",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector}[data-layout="halves"]`,
+						"--grid-placement": "auto-fit",
+						"--grid-min-item-size": "clamp(16rem, 50vw, 33rem)",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector}[data-layout="thirds"]`,
+						"--grid-placement": "auto-fit",
+						"--grid-min-item-size": "clamp(16rem, 33%, 20rem)",
+					};
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				/^repel$/,
+				function* (_, { symbols }) {
+					yield {
+						display: "flex",
+						"flex-wrap": "wrap",
+						"justify-content": "space-between",
+						"align-items": "var(--repel-vertical-alignment, center)",
+						gap: " var(--gutter, var(--space-s-m))",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector}[data-nowrap]`,
+						"flex-wrap": "nowrap",
+					};
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				/^sidebar$/,
+				function* (_, { symbols }) {
+					yield {
+						display: "flex",
+						"flex-wrap": "wrap",
+						gap: "var(--gutter, var(--space-s-l))",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector} > :first-child`,
+						"flex-basis": "var(--sidebar-target-width, 20rem)",
+						"flex-grow": "1",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector} > :last-child`,
+						"flex-basis": 0,
+						"flex-grow": 999,
+						"min-width": "var(--sidebar-content-min-width, 50%)",
+					};
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				/^switcher$/,
+				function* (_, { symbols }) {
+					yield {
+						display: "flex",
+						"flex-wrap": "wrap",
+						gap: "var(--gutter, var(--space-s-l))",
+						"align-items": "var(--switcher-vertical-alignment, flex-start)",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector} > *`,
+						"flex-grow": "1",
+						"flex-basis": "calc((var(--switcher-target-container-width, 40rem) - 100%) * 999)",
+					};
+					yield {
+						[symbols.selector]: (selector) => `${selector} > :nth-child(n + 3)`,
+						"flex-basis": "100%",
+					};
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			[
+				"wrapper",
+				{
+					"margin-inline": "auto",
+					"max-width": "clamp(16rem, var(--wrapper-max-width, 100vw), 80rem)",
+					"padding-left": "var(--gutter)",
+					"padding-right": "var(--gutter)",
+					position: "relative",
+				},
+				{ layer: config.compositionsLayerName },
+			],
+			// utilities
+			[
+				"region",
+				{
+					"padding-block": "var(--region-space, var(--space-xl-2xl))",
+				},
+				{ layer: config.utilitiesLayerName },
+			],
+			[
+				"visually-hidden",
+				{
+					border: "0",
+					clip: "rect(0 0 0 0)",
+					height: "0",
+					margin: "0",
+					overflow: "hidden",
+					padding: "0",
+					position: "absolute",
+					width: "1px",
+					"white-space": "nowrap",
+				},
+				{ layer: config.utilitiesLayerName },
+			],
+			// utility classes
+			[
+				/^flow-space-(.+)$/,
+				([, d]) => ({ "--flow-space": `var(--space-${d})` }),
+				{ layer: config.utilityClassesLayerName },
+			],
+			[
+				/^region-space-(.+)$/,
+				([, d]) => ({ "--region-space": `var(--space-${d})` }),
+				{ layer: config.utilityClassesLayerName },
+			],
+			[
+				/^gutter-(.+)$/,
+				([, d]) => ({ "--gutter": `var(--space-${d})` }),
+				{ layer: config.utilityClassesLayerName },
+			],
+		],
+	};
 }
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,35 @@
 export interface CubePresetOptions {
-  /**
-   * The layer name for CUBE.fyi Block components.
-   * See: https://cube.fyi/block.html
-   * @default "components"
-   */
-  blocksLayerName?: string | undefined;
-  /**
-   * The layer name for CUBE.fyi Composition Layer
-   * See: https://cube.fyi/composition.html
-   * @default "components"
-   */
-  compositionsLayerName?: string | undefined;
-  /**
-   * The layer name for CUBE.fyi Utilities.
-   * @default "components"
-   * See: https://cube.fyi/utility.html
-   */
-  utilitiesLayerName?: string | undefined;
-  /**
-   * The layer name for utility classes (--flow-space, --region-space, --gutter).
-   * @default "components"
-   */
-  utilityClassesLayerName?: string | undefined;
+	/**
+	 * The layer name for CUBE.fyi Block components.
+	 *
+	 * See: https://cube.fyi/block.html
+	 * @default "components"
+	 */
+	blocksLayerName?: string | undefined;
+	/**
+	 * The layer name for CUBE.fyi Composition Layer
+	 *
+	 * See: https://cube.fyi/composition.html
+	 * @default "components"
+	 */
+	compositionsLayerName?: string | undefined;
+	/**
+	 * The layer name for CUBE.fyi Utilities.
+	 *
+	 * @default "components"
+	 * See: https://cube.fyi/utility.html
+	 */
+	utilitiesLayerName?: string | undefined;
+	/**
+	 * The layer name for utility classes (--flow-space, --region-space, --gutter).
+	 *
+	 * @default "components"
+	 */
+	utilityClassesLayerName?: string | undefined;
+	/**
+	 * Utils prefix. When using tagged pseudo selector, only the first truthy prefix will be used.
+	 *
+	 * @default undefined
+	 */
+	prefix?: string | string[] | undefined;
 }
