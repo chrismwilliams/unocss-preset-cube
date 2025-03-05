9f03a6869593753853b6ddf3a7e66c8df0701d4b
Chris Williams <chrismwilliams@users.noreply.github.com>
2025-03-05 14:17:26 +0000

    add design token to playground

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,6 +1,41 @@
+import { clampGenerator, tokensToTailwind as tokensToUno } from "@chriswilliams/tokens-to-tailwind";
 import { defineConfig, presetMini } from "unocss";
 import { presetCube } from "unocss-preset-cube";
 
+import spacingTokens from "./src/css/design-tokens/spacing.json";
+import viewports from "./src/css/design-tokens/viewports.json";
+
+const spacing = tokensToUno(clampGenerator(spacingTokens.items, viewports));
+
 export default defineConfig({
 	presets: [presetMini({ preflight: "on-demand", prefix: "un-" }), presetCube()],
+	preflights: [
+		{
+			// add default gutter
+			getCSS: () => `
+				:root {
+					/* default gutter size */
+					--gutter: var(--space-s-l);
+				}
+			`,
+		},
+		{
+			// add spacing css vars
+			getCSS: () => {
+				let spacingVars = "";
+				for (const [key, value] of Object.entries(spacing)) {
+					spacingVars += `--space-${key}:${value};`;
+				}
+				return `:root {${spacingVars}}`;
+			},
+		},
+	],
+	theme: {
+		spacing,
+		breakpoints: {
+			sm: `${viewports.min}px`,
+			md: `${viewports.mid}px`,
+			lg: `${viewports.max}px`,
+		},
+	},
 });
